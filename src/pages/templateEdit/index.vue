ab84b7ee348e7d615a816db874e2503a28272b0a
walljs <502016@nd.com>
2018-07-18 20:37:39 +0800

    initialize, use mpvue

---
+++ src/pages/templateEdit/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container">
+    <div class="controller">
+      <div class="controller-round">
+        <div class="controller-round-leading">
+          发起一个
+        </div>
+        <div class="controller-round-text">
+          群自我介绍
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import card from '@/components/card'
+
+export default {
+  data () {
+    return {
+      userInfo: {}
+    }
+  },
+
+  components: {
+    card
+  },
+
+  methods: {
+    bindViewTap () {
+      const url = '../logs/main'
+      wx.navigateTo({ url })
+    },
+    getUserInfo () {
+      // 调用登录接口
+      wx.login({
+        success: () => {
+          wx.getUserInfo({
+            success: (res) => {
+              this.userInfo = res.userInfo
+            }
+          })
+        }
+      })
+    }
+  },
+
+  created () {
+    // 调用应用实例的方法获取全局数据
+    // this.getUserInfo()
+  }
+}
+</script>
+
+<style lang="scss">
+.page-template {
+  padding: 60rpx;
+}
+</style>
---
+++ src/pages/templateEdit/main.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+import App from './index'
+
+const app = new Vue(app)
+app.$mount()
